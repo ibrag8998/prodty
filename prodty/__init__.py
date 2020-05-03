33873292b6db5d5b84ca0c181cfb8362d4b72d43
ibrag8998 <ibragdzh@gmail.com>
2020-05-03 23:48:15 +0300

    add config.py and moved here secret key

--- prodty/__init__.py
+++ prodty/__init__.py
@@ -7,10 +7,6 @@ from flask import (
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        #DATABASE=os.path.join(app.instance_path, '')
-    )
 
     if not os.path.exists(app.instance_path):
         os.makedirs(app.instance_path)
