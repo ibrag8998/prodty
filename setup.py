ad3b73fa3e838a926491c5e8ca060141318e3d0a
ibrag8998 <ibragdzh@gmail.com>
2020-05-06 12:11:54 +0300

    made prodty installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='prodty',
+    version='1.0.0',
+    packages=['prodty'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
+
