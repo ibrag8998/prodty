faf0e829777db0f3018834c487ba21d62be9ca58
ibrag8998 <ibragdzh@gmail.com>
2020-05-07 15:32:48 +0300

    registered template filter tstamp_to_dt

--- prodty/__init__.py
+++ prodty/__init__.py
@@ -6,6 +6,7 @@ from flask import render_template
 from . import db
 from . import auth
 from .task import task
+from .task.tstamp import tstamp_to_dt
 
 
 def create_app(test_config=None):
@@ -24,10 +25,11 @@ def create_app(test_config=None):
     db.init_app(app)
     app.register_blueprint(auth.bp)
     app.register_blueprint(task.bp)
-
     # the following line lets us use ``` url_for('index') ```
     # instead of ``` url_for('task.index') ```
     app.add_url_rule('/', endpoint='index')
+
+    app.add_template_filter(tstamp_to_dt, 'todt')
 
     @app.route('/hello')
     def hello():
